fcbd191f889388dec520f9438251a08768761306
7beunseo <kimes0403@gmail.com>
2024-08-15 03:32:34 +0900

    [REMOVE] Sample 관련 코드 모두 삭제 #22

--- src/main/java/com/wiscom/backend/entity/CategoryEnum.java
+++ src/main/java/com/wiscom/backend/entity/CategoryEnum.java
@@ -3,3 +3,4 @@ package com.wiscom.backend.entity;
 public enum CategoryEnum {
     WEB_APP, GAME, AI, BIG_DATA;
 }
+"[REMOVE] Sample 관련 코드 모두 삭제 "
--- src/main/java/com/wiscom/backend/repository/SampleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wiscom.backend.repository;
-
-import com.wiscom.backend.entity.SampleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SampleRepository extends JpaRepository<SampleEntity, Long> {
-}
