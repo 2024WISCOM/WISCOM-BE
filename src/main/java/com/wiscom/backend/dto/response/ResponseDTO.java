72ffd520c93b2c7ad175cef55d6425f0edbfd6bf
7beunseo <kimes0403@gmail.com>
2024-08-20 17:40:29 +0900

    [FIX] 잘못된 import 삭제 #29

--- src/main/java/com/wiscom/backend/dto/response/ResponseDTO.java
+++ src/main/java/com/wiscom/backend/dto/response/ResponseDTO.java
@@ -1,7 +1,6 @@
 package com.wiscom.backend.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.wiscom.backend.dto.sample.SampleResponseDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
